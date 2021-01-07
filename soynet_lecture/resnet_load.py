1e599c4f38ad2abb45994fcf468f9a2efb617a87
elbicuderri <muger1031@gmail.com>
2021-01-07 22:35:42 +0900

    update load

---
+++ soynet_lecture/resnet_load.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class SimpleResNet(nn.Module):
+    def __init__(self):
+        super(SimpleResNet, self).__init__()
+
+        self.relu = nn.ReLU()
+
+        self.conv0 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+
+        self.block11 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(16)
+        )
+
+        self.block12 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(16)
+        )
+
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1, stride=2, bias=False)
+
+        self.block21 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(32)
+        )
+
+        self.block22 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(32)
+        )
+
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride=2, bias=False)
+
+        self.block31 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride=2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(64)
+        )
+
+        self.block32 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1, stride=1, bias=False),
+            nn.BatchNorm2d(64)
+        )
+
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, 10)
+
+    def forward(self, x):
+        batch = x.size(0)
+
+        out0 = self.conv0(x)
+        out1 = self.block11(out0)
+        out1 = self.block12(out1)
+
+        res2 = self.conv2(out1)
+        out2 = self.block21(out1)
+        out2 = self.block22(out2)
+        out2 += res2
+        out2 = self.relu(out2)
+
+        res3 = self.conv3(out2)
+        out3 = self.block31(out2)
+        out3 = self.block32(out3)
+        out3 += res3
+        out3 = self.relu(out3)
+
+        out3 = self.avg_pool(out3)
+        out3 = out3.view(batch, -1)
+        out = self.fc(out3)
+
+        return out
+
+##==================================================
+model = SimpleResNet().to(device)
+model_path = torch.load('model/resnet.pt')
+model.load_state_dict(model_path)
+model.eval()
+for n, w in model.named_parameters():
+    print(n, ": ", w.dtype, ", ", w.size())
+##==================================================
+
+##==================================================
+model = SimpleResNet().to(device)
+checkpoint = torch.load("checkpoint/cifar10_epoch_1.ckpt")
+model.load_state_dict(checkpoint['model_state_dict'])
+optimizer = torch.optim.Adam(model.parameters())
+optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+model.eval()
+for n, w in model.named_parameters():
+    print(n, ": ", w.dtype, ", ", w.size())
+##==================================================
+
+
