997181ecb6f18369b4b0adaa6556089afb6f1864
elbicuderri <muger1031@gmail.com>
2021-01-10 14:20:20 +0900

    delete c

--- soynet_lecture/mnist_c/make_test_bin.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-df = pd.read_csv("mnist_test.csv", delimiter=",", header=None)
-print(df.head(5), type(df), df.shape, df.dtypes)
-df = df.astype("float32")
-print(df.dtypes)
-df = np.array(df)
-df.tofile("mnist_test_float.bin")
