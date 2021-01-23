16e5900104034d5ad8c176b39b6c4218d4e2803b
elbicuderri <muger1031@gmail.com>
2021-01-23 19:33:13 +0900

    study convtranspose

---
+++ convtranspose/convtranspose_myself.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+cifar = torch.randn((1, 3, 32, 32))
+
+"""
+be careful when stride > 1
+"""
+out = nn.ConvTranspose2d(in_channels=3,
+                         out_channels=6,
+                         kernel_size=3,
+                         stride=2,
+                         padding=1,
+                         )(cifar) ## what the???
+
+print(out.size())
+
+out2 = nn.ConvTranspose2d(in_channels=3,
+                         out_channels=6,
+                         kernel_size=3,
+                         stride=2,
+                         padding=1,
+                         output_padding=1,
+                         )(cifar) ## what the???
+
+print(out2.size())
+
+
