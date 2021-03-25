a94f6601c7ce403f3366349b9313fd5b72c9f967
seung.hwan <46323118+elbicuderri@users.noreply.github.com>
2021-03-25 22:32:10 +0900

    Update nms_test.py

--- nms_test_code/nms_test.py
+++ nms_test_code/nms_test.py
@@ -31,8 +31,8 @@ def IOU(box1, box2):
     assert box1_x2 > box1_x1 and box1_y2 > box1_y1
     assert box2_x2 > box2_x1 and box2_y2 > box2_y1 
     
-    if (box2_x1 >= box1_x2 or box1_x1 >= box2_x2):
-        return 0
+    # if (box2_x1 >= box1_x2 or box1_x1 >= box2_x2):
+    #     return 0
 
     left_top_x = max(box1_x1, box2_x1)
     left_top_y = max(box1_y1, box2_y1)
@@ -48,6 +48,7 @@ def IOU(box1, box2):
     
     # right_bottom_x = box1_x2 if box1_x2 <= box2_x2 else box2_x2
     # right_bottom_y = box1_y2 if box1_y2 <= box2_y2 else box2_y2
+    
     
     inter_area = Area([left_top_x, left_top_y, right_bottom_x, right_bottom_y])
     
@@ -97,7 +98,7 @@ def nms_cpu(boxes,
         for box2_idx ,box2 in enumerate(boxes[box1_idx+1:]):
             if IOU(box1, box2) > threshold:
                 # keep.append(box1_idx + box2_idx)
-                keep[box1_idx+box2_idx] += -100
+                keep[box1_idx+box2_idx] = -10000
                 
     # print(keep)
     keep = [ e for e in keep if e > 0 ]
@@ -115,6 +116,9 @@ def generate_bbox(height, width):
     bbox = [x1, y1, x2, y2]
     return bbox
 
+def generate_scores(length):
+    return [ random.uniform(0.001, 0.999) for _ in range(length)]
+
 def check_nms(indices1, indices2):
     if len(indices1) != len(indices2):
         print(f"len(indices1): {len(indices1)}, len(indicese2): {len(indices2)}")
@@ -124,52 +128,34 @@ def check_nms(indices1, indices2):
     Num = len(indices1)
     for n in range(Num):
         if indices1[n] != indices2:
-            print(f"my_nms: {indices1[n]}, but pytorch_nms: {indices2[n]}")
+            print(f"indices1: {indices1[n]}, but indices2: {indices2[n]}")
             print("Wrong!! index!!")
             return False
         
     print("Correct!!")
     return True
 
-# test_data = [[350.9821, 161.8200, 369.9685, 205.2372], 
-#              [250.5236, 154.2844, 274.1773, 204.9810], 
-#              [471.4920, 160.4118, 496.0094, 213.4244], 
-#              [352.0421, 164.5933, 366.4458, 205.9624], 
-#              [166.0765, 169.7707, 183.0102, 232.6606], 
-#              [252.3000, 183.1449, 269.6541, 210.6747], 
-#              [469.7862, 162.0192, 482.1673, 187.0053], 
-#              [168.4862, 174.2567, 181.7437, 232.9379], 
-#              [470.3290, 162.3442, 496.4272, 214.6296], 
-#              [251.0450, 155.5911, 272.2693, 203.3675], 
-#              [252.0326, 154.7950, 273.7404, 195.3671], 
-#              [351.7479, 161.9567, 370.6432, 204.3047],
-#              [496.3306, 161.7157, 515.0573, 210.7200], 
-#              [471.0749, 162.6143, 485.3374, 207.3448], 
-#              [250.9745, 160.7633, 264.1924, 206.8350]]
-
-# test_data_scores = [0.1919, 0.3293, 0.0860, 0.1600, 0.1885, 
-#                     0.4297, 0.0974, 0.2711, 0.1483, 0.1173, 
-#                     0.1034, 0.2915, 0.1993, 0.0677, 0.3217]
 
 test_data = []
-for i in range(53):
+for i in range(5000):
     test_data.append(generate_bbox(500, 500))
     
-# print(test_data)
+test_data_scores = generate_scores(5000)
+    
+cpu_out = nms_cpu(test_data, test_data_scores, 0.5)
 
-test_data_scores = [0.1919, 0.3293, 0.0860, 0.1600, 0.1885, 0.4297, 0.0974, 0.2711,
-      0.1483, 0.1173, 0.1034, 0.2915, 0.1993, 0.0677, 0.3217, 0.0966, 0.0526,
-      0.5675, 0.3130, 0.1592, 0.1353, 0.0634, 0.1557, 0.1512, 0.0699, 0.0545,
-      0.2692, 0.1143, 0.0572, 0.1990, 0.0558, 0.1500, 0.2214, 0.1878, 0.2501,
-      0.1343, 0.0809, 0.1266, 0.0743, 0.0896, 0.0781, 0.0983, 0.0557, 0.0623,
-      0.5808, 0.3090, 0.1050, 0.0524, 0.0513, 0.4501, 0.4167, 0.0623, 0.1749]
+print(cpu_out[:50])
 
-box_indices = nms_cpu(test_data, test_data_scores, 0.5)
+torch_out = torchvision.ops.nms(torch.tensor(test_data), torch.tensor(test_data_scores), iou_threshold=0.5)
 
-print(box_indices)
+print(torch_out[:50])
 
-out = torchvision.ops.nms(torch.tensor(test_data).to('cuda'), torch.tensor(test_data_scores).to('cuda'), iou_threshold=0.5)
+torch_gpu_out = torchvision.ops.nms(torch.tensor(test_data).to('cuda'), torch.tensor(test_data_scores).to('cuda'), iou_threshold=0.5)
 
-print(out)
+print(torch_gpu_out[:50])
 
-check_nms(box_indices, list(out))
+check_nms(cpu_out, list(torch_out))
+print("=======================")
+check_nms(cpu_out, list(torch_gpu_out))
+print("=======================")
+check_nms(list(torch_out), list(torch_gpu_out))
